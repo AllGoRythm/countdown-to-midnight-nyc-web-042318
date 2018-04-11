31f53bea734a888ea454e65988126aed0197807e
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-11 21:00:32 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 #write your code here
 def countdown
       numbers = 10
-      while numbers < 10
+      while numbers > 10
         return "#{numbers} SECONDS(S)!"
         numbers -= 1
       end
